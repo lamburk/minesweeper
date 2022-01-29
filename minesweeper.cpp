fa70efe20d4235486b5beca484c95be471438abf
lamburk <john.constantine365@gmail.com>
2022-01-29 13:58:02 +0530

    First commit

---
+++ minesweeper.cpp
@@ -0,0 +1,13 @@
+/* Write your code here */
+#include <deque>
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+
+// Implement the function fixCounts
+void fixCounts(Grid & mines, Grid & counts);
